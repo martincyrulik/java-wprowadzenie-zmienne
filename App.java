e9607259551b623a00ab67ec7fcb75ff01ecf4d5
martincyrulik <martincyrulik@gmail.com>
2025-01-15 14:33:17 +0100

    dodanie pliku App.java oraz .gitignore

---
+++ App.java
@@ -0,0 +1,30 @@
+class App {
+	public static void main(String args[]) {
+		// zadanie 1
+		System.out.println("-------- Zadanie 1 ---------");
+		int birthYear = 2001;
+		char gender = 'K';
+		
+		System.out.println("BirthYear: " + birthYear);
+		System.out.println("Gender: " + gender);
+		
+		// zadanie 2
+		System.out.println("-------- Zadanie 2 ---------");
+		boolean isAdult = true;
+		double average = 3.5;
+		System.out.println("Is Adult? " + isAdult);
+		System.out.println("Average: " + average);
+		
+		// zadanie 3
+		System.out.println();
+		System.out.println("-------- Zadanie 3 ---------");
+		System.out.println();
+		byte houseNumber = 13;
+		int aptNumber = 127;
+		double bankSavingsAccount = 5120.88;
+		
+		System.out.println("My house number: " + houseNumber);
+		System.out.println("My Apartment number: " + aptNumber);
+		System.out.println("My current bank account: " + bankSavingsAccount);
+	}	
+}
